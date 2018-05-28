9c6c03b45184560f6b6246c201a7a16fe0dacbd3
Minsolt <hukmkms@naver.com>
2018-05-28 17:24:33 +0900

    CL 합침

--- bytruck/src/dao/BoardDAO.java
+++ bytruck/src/dao/BoardDAO.java
@@ -1,10 +1,6 @@
 package dao;
 
 public interface BoardDAO {
-<<<<<<< HEAD
 	public void deleteBoard(int boardNum) throws Exception;
 	
-=======
-
->>>>>>> 70e4d06256aa968735e843b79ecf4834fc817b64
 }
